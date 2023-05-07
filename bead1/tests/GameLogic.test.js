f6866e4a88e562ac1a18c420d33df77d3159392e
Roland Papdi <rolee15@gmail.com>
2023-05-07 19:37:55 +0200

    Fixed failing tests

--- bead1/tests/GameLogic.test.js
+++ bead1/tests/GameLogic.test.js
@@ -1,7 +1,8 @@
 import GameLogic from "../GameLogic.js";
 
-test("should create a board with give board size", () => {
+test("should create a board with given board size", () => {
   const size = 6;
   let game = new GameLogic(size);
-  expect(game.board).toBe(size);
+  expect(game.board.length).toBe(size);
+  expect(game.board[0].length).toBe(size);
 });

92bd3c4344e456e6f2278054439f028cffebf9fc
Roland Papdi <rolee15@gmail.com>
2023-06-18 22:16:11 +0200

    Fixed profile links

--- php-bead/public/playlist.php
+++ php-bead/public/playlist.php
@@ -56,7 +56,7 @@ $tracks = $res->fetchAll(PDO::FETCH_ASSOC);
         <div>
             <?php
             if (isset($_SESSION['user'])) {
-                echo '<span><a href="/profile.php?username=' . $_SESSION['user'] . '" class="nostyle">' . $_SESSION['user'] . '</a></span>';
+                echo '<span><a href="/profile.php" class="nostyle">' . $_SESSION['user'] . '</a></span>';
                 echo '<span><a href="/logout.php?redirect=' . $link . '" class="nostyle">Logout</a></span>';
             } else {
                 echo '<span><a href="/login.php?redirect=' . $link . '" class="nostyle">Login</a></span>';
--- php-bead/public/profile.php
+++ php-bead/public/profile.php
@@ -41,7 +41,7 @@ if (!is_array($user)) {
         <div>
             <?php
             if (isset($_SESSION['user'])) {
-                echo '<span><a href="/profile.php?username=' . $_SESSION['user'] . '" class="nostyle">' . $_SESSION['user'] . '</a></span>';
+                echo '<span><a href="/profile.php" class="nostyle">' . $_SESSION['user'] . '</a></span>';
                 echo '<span><a href="/logout.php?redirect=' . $link . '" class="nostyle">Logout</a></span>';
             } else {
                 echo '<span><a href="/login.php?redirect=' . $link . '" class="nostyle">Login</a></span>';
